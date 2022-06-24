591b6d86c88cbb418c7f9445365a9987c1a5f829
ra6yee <ra6yee@mail.ru>
2022-06-24 22:44:30 +0300

    4 7

--- src/Test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/Test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -14,9 +14,8 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import static UserTestData.NOT_FOUND;
 import static UserTestData.USER_ID;
 
-@ContextConfiguration("classpath:spring/spring-app.xml")
+@ContextConfiguration({"classpath:spring/spring-app.xml", "classpath:spring/inmemory.xml"})
 @RunWith(SpringRunner.class)
-@Ignore
 public class InMemoryAdminRestControllerSpringTest {
 
     @Autowired
